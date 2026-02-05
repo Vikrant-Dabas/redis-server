742630cb08a1713456fcfd3aab2e0a50745a30f5
Vikrant Dabas <dabasvikrant10@gmail.com>
2026-02-05 19:20:35 +0530

    reset changes

--- resp/unmarshal.go
+++ resp/unmarshal.go
@@ -57,3 +57,59 @@ func (f *Format)ArrayUnmarshal(r *bufio.Reader,payload []byte) error{
 	}
 	return nil
 }
+
+func contains(b []byte) (byte,bool){
+	for _,i := range b{
+		for _,ch := range AllTypes{
+			if i == ch{
+				return ch,true
+			}
+		}
+	}
+	return 0,false;
+}
+
+func validTerminator(b []byte)bool{
+	n := len(b)
+	if n < 2{
+		return false
+	}
+	if b[n-2] != '\r' || b[n-1] != '\n'{
+		return false
+	}
+
+	for _,ch := range b[:n-2]{
+		if ch == '\r' || ch == '\n'{
+			return false
+		}
+	}
+	return true
+}
+
+func terminatorInBetween(b []byte)bool{
+	for _,ch := range b{
+		if ch == '\r' || ch == '\n'{
+			return true
+		}
+	}
+	return false
+}
+
+func readValidateInput(r *bufio.Reader)([]byte,error){
+	payload,err := r.ReadBytes('\n')	
+	fmt.Printf("\033[94m%s-%d\033[0m\n",payload,len(payload))
+	if err != nil{
+		return nil,err
+	}
+	if(!validTerminator(payload)){
+		return nil,fmt.Errorf("syntax error: terminator: %d-%s\n",len(payload),payload)
+	}
+	if _,ok := contains([]byte{payload[0]});ok{
+		if ch,ok := contains(payload[1:]);ok{
+			return nil,fmt.Errorf("syntax error: unexpted message type - %c",ch)
+		}
+	} else if ch,ok := contains(payload);ok{
+		return nil,fmt.Errorf("syntax error: unexpted message type - %c",ch)
+	}
+	return payload,nil
+}
